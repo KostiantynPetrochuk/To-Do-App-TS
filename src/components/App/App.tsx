41377f068b1ba39b71165ad4245f0f2d020b7895
Kostiantyn <firstpickpudge@gmail.com>
2022-12-07 20:03:12 +0200

    added @emotion modules

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,10 @@
-// import SimpleContainer from '../SimpleContainer';
+import SimpleContainer from '../SimpleContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <SimpleContainer /> */}
-      <h3>Hello World! ^_^</h3>
+      <SimpleContainer />
     </div>
   );
 }
